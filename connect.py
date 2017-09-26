f2525c3dc55d66f974aca9d7fbbbd556a59c5d6a
risleychris <risley.c@husky.neu.edu>
2017-09-26 14:18:50 -0400

    Initial commit

---
+++ connect.py
@@ -0,0 +1,69 @@
+import bitstamp.client
+
+
+class Bit_Connect:
+        key = ""
+        secret = ""
+        customer_id = ""
+
+        def __init__(self, key, secret, customer_id):
+            self.key = key
+            self.secret = secret
+            self.customer_id = customer_id
+
+        # Returns the private client  api access
+        def get_private_client(self):
+            trading_client = bitstamp.client.Trading(
+            username=self.customer_id, key=self.key, secret=self.secret)
+            return trading_client
+
+        # Returns the public client api access
+        def get_public_client(self):
+            return bitstamp.client.Public()
+
+        def get_account_balance(self, currency_type, quote):
+            client = self.get_private_client()
+            return client.account_balance(currency_type, quote)
+
+        # Returns the value of the given currency
+        def get_market_price(self, currency_type, quote):
+            client = self.get_public_client()
+            return client.ticker(currency_type, quote)["last"]
+
+        # Returns the value of the given currency
+        def get_percent_change(self, currency_type, quote):
+            client = self.get_public_client()
+            return (1 - float(client.ticker(currency_type, quote)["low"])
+                    / float(client.ticker(currency_type, quote)["last"])) * 100
+
+        # Perform a market buy
+        def market_buy(self, amount, currency_type, quote):
+            client = self.get_private_client()
+            client.buy_market_order(amount, currency_type, quote)
+            return "Market Buy Initiated on " + currency_type
+
+        # Perform a market sell
+        def market_sell(self, amount, currency_type, quote):
+            client = self.get_private_client()
+            client.sell_market_order(amount, currency_type, quote)
+            return "Market Sell Initiated on " + currency_type
+
+        # Perform a limit buy
+        def limit_buy(self, amount, price, currency_type, quote):
+            client = self.get_private_client()
+            client.buy_limit_order(amount, price, currency_type, quote)
+            return "Limit Buy Initiated on " + currency_type + "at " + price
+
+        # Perform a market buy
+        def limit_sell(self, amount, price, currency_type, quote):
+            client = self.get_private_client()
+            client.sell_limit_order(amount, price, currency_type, quote)
+            return "Limit Sell Initiated on " + currency_type + "at " + price
+
+        def cancel_orders(self):
+            client = self.get_private_client()
+            client.cancel_all_orders()
+            return "Orders Cancelled"
+
+
+
---
+++ run.py
@@ -0,0 +1,63 @@
+from connect import Bit_Connect
+import time
+
+connect = Bit_Connect(key, secret, customer_id)
+
+# First buy in
+# Set lower bound at buy in price
+# Continue holding
+# If price increases by 10%
+# move lower bound up by %5
+# if price reaches lower bound, sell
+# If price decreases by 10%, move lower bound down by 5%
+# If price reaches lower bound and you own no currency, buy in
+
+# price = 260  buy in
+# price increases to 300, increase lower bound to 280
+# if price decreases to 280, sell
+# if price continues to decrease to 220 (-10%), decrease lower bound to 240
+# if price increases back to 240, buy in
+
+
+def run_script():
+    log = ""
+    eth_lower_bound = 260
+    holding = False
+
+    while True:
+        time.sleep(2)
+        current_value = connect.get_market_price('eth', 'usd')
+        moving_balance = connect.get_account_balance('eth', 'usd')['eth_balance'] / 10
+
+        if current_value == eth_lower_bound and holding:
+            log += "Lower Bound Reached, Selling Moving Balance" + '\n'
+            connect.limit_sell(moving_balance, eth_lower_bound, 'eth', 'usd')
+            holding = False
+        if current_value == eth_lower_bound and not holding:
+            log += "Lower Bound Reached, Buying Moving Balance" + '\n'
+            connect.limit_buy(moving_balance, eth_lower_bound, 'eth', 'usd')
+            holding = True
+        if connect.get_percent_change('eth', 'usd') > 10:
+            new_bound =  eth_lower_bound + eth_lower_bound * .5
+            log += "Raising ETH Lower Bound by 5% from" + str(eth_lower_bound) + " to " + str(new_bound) + '\n'
+            log += "Current Value is at " + str(current_value)
+            eth_lower_bound = new_bound
+        if connect.get_percent_change('eth', 'usd') < -10:
+            new_bound =  eth_lower_bound - eth_lower_bound * .5
+            log += "Lowering ETH Lower Bound by 5% from" + str(eth_lower_bound) + " to " + str(new_bound) + '\n'
+            log += "Current ETH Value is at " + str(current_value)
+            eth_lower_bound = new_bound
+
+if __name__ == '__main__':
+    run_script()
+
+
+
+
+
+
+
+
+
+
+
