f5734cc21843b19a697f42550725773e90f9cd4a
risleychris <risley.c@husky.neu.edu>
2017-09-28 11:48:41 -0400

    Fixed multiple issues

--- src/run.py
+++ src/run.py
@@ -110,6 +110,7 @@ class EthereumAlgorithms:
         # Runs the script until program quits
         t_data = self.connect.retrieve_transaction_history()
         current_value = float(self.connect.get_market_price('eth', 'usd'))
+        num_of_calls = 1
         try:
             switch_bound = float(t_data[0]["eth_usd"])
         except:
@@ -117,11 +118,13 @@ class EthereumAlgorithms:
 
         try:
             eth_balance = round(float(self.connect.get_account_balance('eth', 'usd')['eth_balance']), 6)
+            num_of_calls += 1
         except:
             eth_balance = round(float(input("Authorization Failed. Enter Your Current ETH Balance Manually:")), 6)
 
         try:
             usd_balance = round(float(self.connect.get_account_balance('eth', 'usd')['usd_balance']), 6)
+            num_of_calls += 1
         except:
             usd_balance = round(float(input("Authorization Failed. Enter Your Current USD Balance Manually:")), 6)
 
@@ -141,21 +144,28 @@ class EthereumAlgorithms:
         starting_time = str(datetime.datetime.utcnow())
         new_file = open("log-" + starting_time + ".txt", "w+")
 
+
+
         while True:
             # Make the system sleep to prevent API overuse
             time.sleep(4)
             # Current value of eth
             try:
                 current_value = float(self.connect.get_market_price('eth', 'usd'))
+                num_of_calls += 1
             except:
                 current_value = current_value
+
             percent_change = (1 - starting_value / current_value) * 100
             # Print Info
 
             print ConsoleColors.OKBLUE + str(datetime.datetime.utcnow()) + "\t|| %^:" + str(round(percent_change)) \
-                  + "\t|| ETH:" + str(round(current_value, 2)) + "\t|| Switch Bound:" + str(round(switch_bound, 2)) + "\t|| ETH Balance:" \
-                  + str(round(eth_balance, 2)) + "\t|| USD Balance:" + str(round(usd_balance, 2)) + "\t|| Moving Balance = " \
-                  + str(moving_balance)+ "\t|| Holding = " \
+                  + "\t|| ETH:" + str(round(current_value, 2)) + "\t|| Switch Bound:" + str(
+                round(switch_bound, 2)) + "\t|| ETH Balance:" \
+                  + str(round(eth_balance, 2)) + "\t|| USD Balance:" + str(
+                round(usd_balance, 2)) + "\t|| Moving Balance:" \
+                  + str(moving_balance) + "\t|| API Calls:" \
+                  + str(num_of_calls) + "\t|| Holding = " \
                   + str(holding) + ConsoleColors.ENDC
 
             # Log Info
@@ -167,6 +177,7 @@ class EthereumAlgorithms:
                 "eth_balance": str(eth_balance),
                 "usd_balance": str(usd_balance),
                 "holding": str(holding),
+                "api_calls": str(num_of_calls),
                 "moving_balance": str(moving_balance)}
 
             new_file.write(json.dumps(log_object) + "\n")
@@ -175,9 +186,11 @@ class EthereumAlgorithms:
             if current_value <= switch_bound and holding:
                 # Cancel existing orders and then sell
                 # self.connect.cancel_orders()
+                # num_of_calls += 1
                 self.log += "Switch Bound Reached, Selling Moving Balance" + '\n'
                 print ConsoleColors.WARNING + "Switch Bound Reached, Selling Moving Balance" + ConsoleColors.ENDC
-                # print ConsoleColors.WARNING + json.dumps(self.connect.market_sell(round(moving_balance, 6), 'eth', 'usd')) + ConsoleColors.ENDC
+                # print ConsoleColors.WARNING +
+                # json.dumps(self.connect.market_sell(round(moving_balance, 6), 'eth', 'usd')) + ConsoleColors.ENDC
                 holding = False
                 # Update USD and ETH Balance
                 eth_balance -= moving_balance
@@ -190,6 +203,7 @@ class EthereumAlgorithms:
             elif current_value >= switch_bound and not holding:
                 # Cancel existing orders and then buy
                 # self.connect.cancel_orders()
+                # num_of_calls += 1
                 self.log += "Switch Bound Reached, Buying Moving Balance" + '\n'
                 print("Switch Bound Reached, Buying Moving Balance")
                 moving_balance = usd_balance / current_value
@@ -231,6 +245,7 @@ class EthereumAlgorithms:
 
 
 if __name__ == '__main__':
-    algo = EthereumAlgorithms(2.5, .85, key, secret, customer_id)
+
+    algo = EthereumAlgorithms(1, .85, key, secret, customer_id)
     # algo.test_wrench(False)
     algo.full_wrench(False)
